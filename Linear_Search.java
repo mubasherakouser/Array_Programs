be20ed3cd794821d61f91a86245fac531d6f0bb7
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2025-02-02 14:45:36 +0530

    Linear_Search.java

---
+++ Linear_Search.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Arrays_P {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size:");
+        int size = sc.nextInt();
+        int a[] = new int[size];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        int n=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<=a.length-1;i++) {
+        	if(a[i]==n) {
+        		System.out.println("found");
+        		found=true;
+        		break;
+        	}	
+        }
+        if(found==false) {
+        	System.out.println("not found");
+        }
+}
+}
