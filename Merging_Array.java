0fd1b1a6c543d361bb159aae4b783728f38de4f7
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2025-02-02 14:47:08 +0530

    Merging_Array.java

---
+++ Merging_Array.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Arrays_P{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter sizeof 1st array");
+	int size1=sc.nextInt();
+	int a[]=new int[size1];
+	System.out.println("Enter array elements");
+	for(int i=0;i<=a.length-1;i++){
+	 a[i]=sc.nextInt();
+}
+	System.out.println("enter sizeof 1st array");
+	int size2=sc.nextInt();
+	int b[]=new int[size2];
+	System.out.println("Enter array elements");
+	for(int i=0;i<=b.length-1;i++){
+	 b[i]=sc.nextInt();
+	 }
+	int c[]=ismerge(a,b);
+			System.out.println(Arrays.toString(c));
+	}
+	static int[] ismerge(int a[],int b[]) {
+		int c[]=new int[a.length+b.length];
+		for(int i=0;i<=a.length-1;i++) {
+			c[i]=a[i];
+	}
+		for(int i=0;i<=b.length-1;i++) {
+			c[a.length+i]=b[i];
+		}
+		return c;
+}
+}
