fe930ba5dac58a69850a377eefa5188330bbcc37
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2025-02-04 19:39:23 +0530

    Longest_SubArray_Sum.java

--- Longest_SubArray_Sum.java
+++ Longest_SubArray_Sum.java
@@ -6,18 +6,32 @@ public class SubArray {
 	        System.out.print("Enter the number of elements in the array: ");
 	        int n = scanner.nextInt();
 	        int[] arr = new int[n];
+            int big=0,x=0,y=0;
 	        System.out.println("Enter the elements of the array:");
 	        for (int i = 0; i < n; i++) {
 	            arr[i] = scanner.nextInt(); 
 	        }
-	        System.out.println("All possible subarrays:");
 	        for (int i = 0; i < n; i++) { 
+	        	int sum=0;
 	            for (int j = i; j < n; j++) { 
+	                sum=sum+arr[j];
 	                for (int k = i; k <= j; k++) { 
-	                    System.out.print(arr[k] + " ");
+	                    //System.out.print(arr[k] + " ");
 	                }
-	                System.out.println(); 
+	              // System.out.println(); 
+	            if(big<sum) {
+	            	big=sum;
+	            	x=i;
+	            	y=j;
 	            }
 	        }
+	        }
+	        System.out.println("Longest Sub Array : ");    
+	        for(int i=x;i<=y;i++) {
+	        	System.out.print(arr[i]+" ");
+	        }
+	        System.out.println();
+	        System.out.println("Big : "+big);
 	    }
-	}
+}
+
